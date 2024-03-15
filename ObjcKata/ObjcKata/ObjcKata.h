9b8da666754018b2c11155f442abbca00ab25f77
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-03-15 10:18:51 +0200

    Add initial project

---
+++ ObjcKata/ObjcKata/ObjcKata.h
@@ -0,0 +1,18 @@
+//
+//  ObjcKata.h
+//  ObjcKata
+//
+//  Created by Volodymyr Myroniuk on 15.03.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ObjcKata.
+FOUNDATION_EXPORT double ObjcKataVersionNumber;
+
+//! Project version string for ObjcKata.
+FOUNDATION_EXPORT const unsigned char ObjcKataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjcKata/PublicHeader.h>
+
+
